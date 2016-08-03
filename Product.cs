8262d51eef3379e2190398df27883cc6868af8cc
YogeshwariBhurley <yogeshwaribhurley@gmail.com>
2016-08-03 17:39:34 +0530

    Update Product.cs
    added comments

--- Product.cs
+++ Product.cs
@@ -7,6 +7,7 @@ namespace BillingApp.Models
 {
     public class Product
     {
+        //Defining Product entities
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
